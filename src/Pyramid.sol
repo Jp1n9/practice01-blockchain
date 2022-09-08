32338622cabf200ee466ba1460b7b2cddaa3fd70
beomjoon <beomjoon.kim@dream-academy.io>
2022-09-08 16:57:51 +0900

    star

--- src/Pyramid.sol
+++ src/Pyramid.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+
+// import "forge-std/console.col";
 contract Pyramid {
     function run(uint size) pure public returns (string memory _out) {
         _out = ""; // TODO
+        for(uint i=0;i<size;i+=1){
+            for(uint j=0;j<=i;j+=1) {
+                _out = string.concat(_out,"*");
+            }
+            _out=string.concat(_out,"\n");
+        }
+
+        // console.log("Returns: \n%s",_out);
         /* Example (size = 5):
          *
          *     "*\n"
